1ccc47a8cfde28ac6941e5cffc08e2c5647ed0db
HZ <zhaihaohk@gmail.com>
2022-06-27 23:58:09 +1200

    Update gas request url

--- packages/network-clients/src/utils/network.ts
+++ packages/network-clients/src/utils/network.ts
@@ -1,25 +1,24 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { BigNumber } from 'ethers';
 import axios from 'axios';
 
-const evmHost = 'http://ec2-13-211-57-210.ap-southeast-2.compute.amazonaws.com';
+const evmHost = 'https://sq-airdrop-backend.thechaindata.com';
 
 type GasLevel = 'high' | 'low';
 
-type Overrides = {
-  gasLimit: BigNumber;
-  gasPrice: BigNumber;
+type EthGas = {
+  gasLimit: string;
+  gasPrice: string;
 };
 
-export async function getEthGas(level: GasLevel = 'low'): Promise<Overrides> {
+export async function getEthGas(level: GasLevel = 'low'): Promise<EthGas> {
   try {
-    const url = `${evmHost}/eth-gas?level=${level}`;
+    const url = `${evmHost}/evm/gas?level=${level}`;
     const res = await axios.get(url);
     const result = await res.data;
 
-    return result as Overrides;
+    return result as EthGas;
   } catch (e) {
     throw Error('Failed to get gas config');
   }
