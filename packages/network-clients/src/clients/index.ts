9f2784eb4abf07f3ba7c56c29712f58d240be960
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-09-22 11:54:22 +1200

    change order of exports

--- packages/network-clients/src/clients/index.ts
+++ packages/network-clients/src/clients/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+export * from './networkClient';
 export * from './contractClient';
 export * from './ipfsClient';
-export * from './networkClient';
 export * from './queryClient';
