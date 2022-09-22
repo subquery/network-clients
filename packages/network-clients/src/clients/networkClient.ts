4fe48d2c56051f7cf8fcba8dc6ecb0c63ebb9920
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-09-22 15:24:33 +1200

    add parentheses for clarity

--- packages/network-clients/src/clients/networkClient.ts
+++ packages/network-clients/src/clients/networkClient.ts
@@ -92,7 +92,7 @@ export class NetworkClient {
 
     const maxUnstakeAmount = min(
       ownStakeAfter.sub(minStakingAmount),
-      ownStakeAfter.mul(leverageLimit).sub(totalStakingAmountAfter).div(leverageLimit.sub(1))
+      (ownStakeAfter.mul(leverageLimit).sub(totalStakingAmountAfter)).div(leverageLimit.sub(1))
     );
 
     return maxUnstakeAmount.isNegative() ? BigNumber.from(0) : maxUnstakeAmount;
