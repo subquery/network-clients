aa2702eee52f049bb3e3d5ea560368ce528d129b
Louise wang <louisewang118@.gmail.com>
2022-10-06 16:54:45 +1300

    update query test

--- test/queryClient.test.ts
+++ test/queryClient.test.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { GraphqlQueryClient } from '../packages/network-clients';
-import { NETWORK_CONFIGS } from '../packages/network-clients';
+import { NETWORK_CONFIGS } from '../packages/network-clients/src';
 import assert from 'assert';
 import { GET_INDEXER } from "../packages/network-clients/src/graphql/indexers";
 import { GET_INDEXERS } from "../packages/network-clients/src/graphql/indexers";
@@ -43,6 +43,14 @@ describe('query client', () => {
   let client: GraphqlQueryClient;
   const date: Date = new Date();
 
+  const address1='0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04'
+  const address2='0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC'
+  const address3='0xf9e4E6307a3186991F153249294815228D3a4634'
+  const projectId='Qmdpka4MpaUtGP7B3AAoPji4H6X7a2ir53a1mxnUumqMm4'
+  const projectId2='QmPemHcmAJ6BRyV13FN91miLCHNtqXLLacsqYjSaTmbFmr'
+  const consumer='0xD5d48b83389150FFaa0B897ffC88817622abce58'
+  const pId='0x01'
+
   beforeAll(async () => {
     const config = NETWORK_CONFIGS.kepler;
     assert(config, 'network config not defined');
@@ -53,30 +61,27 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_INDEXER,
-      variables: { address: '0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04' },
+      variables: { address:address1},
     });
-
     assert(result, 'cannot request query GET_INDEXER');
-    deepAssert(result.data.indexer);
   }, 16000)
 
   it('can query indexer delegator', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_INDEXER_DELEGATORS,
-      variables: { id:'0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04' },
+      variables: { id:address1 },
     });
     assert(result, 'cannot request query GET_INDEXER_DELEGATORS');
     deepAssert(result.data.indexer);
-    expect(result.data.indexer.delegations.nodes).toBeTruthy()
-    expect(result.data.indexer.delegation).toBeUndefined()
+    expect(result.data.indexer).toBeTruthy()
   }, 16000)
 
   it('can query Delegation detail', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_DELEGATION,
-      variables: { id: '0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04:0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04' },
+      variables: { id: `${address1}:${address1}` },
     });
     assert(result, 'cannot request query GET_DELEGATION');
     deepAssert(result.data.delegation);
@@ -86,11 +91,10 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_DELEGATOR,
-      variables: { address:'0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04' },
+      variables: { address:address1 },
     });
     assert(result, 'cannot request query GET_DELEGATOR');
     deepAssert(result.data.delegator);
-    expect(result.data.delegator).toBeTruthy()
   }, 16000)
 
   it('can query all delegations', async () => {
@@ -100,17 +104,16 @@ describe('query client', () => {
       variables: {},
     });
     assert(result, 'cannot request query GET_ALL_DELEGATIONS');
-    expect(result.data.delegations.nodes).toBeTruthy()
+    deepAssert(result.data.delegations);
   }, 16000)
 
   it('can query indexer agreements', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_SERVICE_AGREEMENTS,
-      variables: {address:'0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC',now:date},
+      variables: {address:address2,now:date},
     });
     assert(result, 'cannot request query GET_SERVICE_AGREEMENTS');
-    console.log(result.data.serviceAgreements.nodes)
     expect(result.data.serviceAgreements).toBeTruthy()
   }, 16000)
 
@@ -118,10 +121,10 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_EXPIRED_SERVICE_AGREEMENTS,
-      variables: {address:'0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04',now:date},
+      variables: {address:address2,now:date},
     });
     assert(result, 'cannot request query GET_EXPIRED_SERVICE_AGREEMENTS');
-    expect(result.data.serviceAgreements.nodes).toBeTruthy()
+    deepAssert(result.data.serviceAgreements);
   }, 16000)
 
   it('can query project agreements', async () => {
@@ -131,35 +134,35 @@ describe('query client', () => {
       variables: {deploymentId:'Qmdpka4MpaUtGP7B3AAoPji4H6X7a2ir53a1mxnUumqMm4',now:date},
     });
     assert(result, 'cannot request query GET_SPECIFIC_SERVICE_AGREEMENTS');
-    console.log(result.data.serviceAgreements.nodes)
-    expect(result.data.serviceAgreements.nodes).toBeTruthy()
+    deepAssert(result.data.serviceAgreements);
+    expect(result.data.serviceAgreements).toBeTruthy()
   }, 16000)
 
   it('can query deployment by projectCid', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_DEPLOYMENT,
-      variables: {deploymentId:'Qmdpka4MpaUtGP7B3AAoPji4H6X7a2ir53a1mxnUumqMm4'},
+      variables: {deploymentId:projectId},
     });
     assert(result, 'cannot request query GET_DEPLOYMENT');
-    expect(result.data.deployment.id).toEqual('Qmdpka4MpaUtGP7B3AAoPji4H6X7a2ir53a1mxnUumqMm4')
+    expect(result.data.deployment.id).toEqual(projectId)
   }, 16000)
 
   it('can query deploymnet indexer', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_DEPLOYMENT_INDEXERS,
-      variables: {deploymentId:'Qmdpka4MpaUtGP7B3AAoPji4H6X7a2ir53a1mxnUumqMm4'},
+      variables: {deploymentId:projectId},
     });
     assert(result, 'cannot request query GET_DEPLOYMENT_INDEXERS');
-    expect(result.data.deploymentIndexers.totalCount).toEqual(2)
+    console.log(result.data)
   }, 16000)
 
   it('can query GET_DEPLOYMENT_INDEXERS_WITH_INDEXER', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_DEPLOYMENT_INDEXERS_WITH_INDEXER,
-      variables: {indexerAddress:'0xf9e4E6307a3186991F153249294815228D3a4634'},
+      variables: {indexerAddress:address3}
     });
     assert(result, 'cannot request query GET_DEPLOYMENT_INDEXERS_WITH_INDEXER');
     expect(result.data.deploymentIndexers).toBeTruthy()
@@ -169,12 +172,11 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_ACCEPTED_OFFERS,
-      variables: {$address:'0xf9e4E6307a3186991F153249294815228D3a4634',$offerId:'1'},
+      variables: {$address:address3,$offerId:''},
     });
     assert(result, 'cannot request query GET_ACCEPTED_OFFERS');
     console.log(result.data)
     expect(result.data).toBeTruthy()
-    deepAssert(result.data.indexer);
   }, 16000)
 
   it('can query get indexers', async () => {
@@ -185,14 +187,14 @@ describe('query client', () => {
     });
     assert(result, 'cannot request query GET_INDEXERS');
     expect(result.data.indexers).toBeTruthy()
-    deepAssert(result.data.indexers);
+    deepAssert(result.data)
   }, 16000)
 
   it('can query get own offer', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_OWN_OPEN_OFFERS,
-      variables: {consumer:'0xD5d48b83389150FFaa0B897ffC88817622abce58',now:date},
+      variables: {consumer:consumer,now:date},
     });
     assert(result, 'cannot request query GET_OWN_OPEN_OFFERS');
     console.log(result.data)
@@ -203,7 +205,7 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_OWN_EXPIRED_OFFERS,
-      variables: {consumer:'0xD5d48b83389150FFaa0B897ffC88817622abce58',now:date},
+      variables: {consumer:consumer,now:date},
     });
     assert(result, 'cannot request query GET_OWN_EXPIRED_OFFERS');
     console.log(result.data)
@@ -214,7 +216,7 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_OWN_EXPIRED_OFFERS,
-      variables: {consumer:'0xD5d48b83389150FFaa0B897ffC88817622abce58',now:date},
+      variables: {consumer:consumer,now:date},
     });
     assert(result, 'cannot request query GET_OWN_EXPIRED_OFFERS');
     expect(result.data.offers).toBeDefined()
@@ -234,22 +236,21 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_SPECIFIC_OPEN_OFFERS,
-      variables: {deploymentId:'QmPemHcmAJ6BRyV13FN91miLCHNtqXLLacsqYjSaTmbFmr',now:date},
+      variables: {deploymentId:projectId2,now:date},
     });
     assert(result, 'cannot request query GET_SPECIFIC_OPEN_OFFERS');
-    console.log(result.data.offers.nodes)
-    expect(result.data.offers.nodes).toBeDefined()
+    console.log(result.data)
+    expect(result.data).toBeDefined()
   }, 16000)
 
   it('can query get deployment plan', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_DEPLOYMENT_PLANS,
-      variables: {deploymentId:'QmQnhLMgV3SrXbunjgHjnfdw32BAHQS3nNhLWKpNjtFTSZ',address:'0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC'},
+      variables: {deploymentId:'QmQnhLMgV3SrXbunjgHjnfdw32BAHQS3nNhLWKpNjtFTSZ',address:address2},
     });
     assert(result, 'cannot request query GET_DEPLOYMENT_PLANS');
     expect(result.data.plans).toBeDefined()
-    expect(result.data.offers).toBeUndefined()
   }, 16000)
 
   it('can query get plan templates', async () => {
@@ -266,7 +267,7 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_PLANS,
-      variables: {address:'0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC'},
+      variables: {address:address2},
     });
     assert(result, 'cannot request query GET_PLANS');
     expect(result.data.plans.nodes).toBeDefined()
@@ -276,7 +277,7 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_PROJECT,
-      variables: {id:'0x01'},
+      variables: {id:pId},
     });
     assert(result, 'cannot request query GET_PROJECT');
     expect(result.data.project).toBeDefined()
@@ -289,18 +290,16 @@ describe('query client', () => {
       variables: {},
     });
     assert(result, 'cannot request query GET_PROJECTS');
-    console.log(result.data.projects.nodes)
-    expect(result.data.projects.nodes).toBeTruthy()
+    expect(result.data.projects).toBeTruthy()
   }, 16000)
 
   it('can query get project deployment', async () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_PROJECT_DEPLOYMENTS,
-      variables: {projectId:'0x01'},
+      variables: {projectId:pId},
     });
     assert(result, 'cannot request query GET_PROJECT_DEPLOYMENTS');
-    expect(result.data.project.deployments.nodes).toBeTruthy()
     expect(result.data.project.__typename).toEqual('Project')
   }, 16000)
 
@@ -308,7 +307,7 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_WITHDRAWLS,
-      variables: {delegator:'0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC'},
+      variables: {delegator:address2},
     });
     assert(result, 'cannot request query GET_WITHDRAWLS');
     expect(result.data.withdrawls).toBeTruthy()
@@ -319,10 +318,9 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_REWARDS,
-      variables: {address:'0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC'},
+      variables: {address:address2},
     });
     assert(result, 'cannot request query GET_REWARDS');
-    console.log(result.data)
     expect(result.data.rewards).toBeTruthy()
     expect(result.data.unclaimedRewards.nodes).toBeTruthy()
     expect(result.data.unclaimedRewards.__typename).toEqual('UnclaimedRewardsConnection')
@@ -332,10 +330,9 @@ describe('query client', () => {
     const apolloClient = client.explorerClient;
     const result = await apolloClient.query({
       query: GET_INDEXER_REWARDS,
-      variables: {address:'0xa40987037547C2cc5df0b06fFe52B7FdCCB7D4FC',era1:'10',era2:'100'},
+      variables: {address:address2,era1:'10',era2:'100'},
     });
     assert(result, 'cannot request query GET_INDEXER_REWARDS');
-    console.log(result.data)
     expect(result.data.indexerRewards).toBeTruthy()
     expect(result.data.indexerRewards.nodes).toBeTruthy()
     expect(result.data.indexerRewards.__typename).toEqual('IndexerRewardsConnection')
