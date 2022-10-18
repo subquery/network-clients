88c45c111dd1c783ab19dd0c8c1e5a7231f1e282
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-10-18 15:30:55 +1300

    include codegen.ts and generate the types and js query functions, export them for public use.

--- packages/network-query/src/index.ts
+++ packages/network-query/src/index.ts
@@ -1,3 +1,6 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import * as GraphqlTypes from './__graphql__'
+
+export default GraphqlTypes;
