7bdb802b9351e3fe2b9fb5c4c0c608e7ae824a5d
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-10-18 15:39:27 +1300

    fix network-query export

--- packages/network-query/src/index.ts
+++ packages/network-query/src/index.ts
@@ -1,6 +1,4 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import * as GraphqlTypes from './__graphql__'
-
-export default GraphqlTypes;
+export * from './__graphql__';
