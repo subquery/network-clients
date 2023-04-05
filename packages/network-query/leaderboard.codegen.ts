eb2aa6f898be9a1aef60b536d9f7d645347b4a1b
Louise wang <louisewang118@.gmail.com>
2023-04-05 16:48:29 +1200

    try amend rm file

---
+++ packages/network-query/leaderboard.codegen.ts
@@ -0,0 +1,43 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { CodegenConfig } from '@graphql-codegen/cli';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: CodegenConfig = {
+  schema: process.env.KEPLER_LEADERBOARD,
+  documents: './queries/leaderboard/*.gql',
+  config: {
+    preResolveTypes: true,
+    namingConvention: 'keep',
+    avoidOptionals: true,
+    nonOptionalTypename: true,
+    skipTypeNameForRoot: true,
+    immutableTypes: true,
+    scalars: {
+      Date: 'Date',
+      Datetime: 'Date',
+      BigFloat: 'bigint' || 'string',
+      BigInt: 'bigint',
+      Cursor: 'string',
+    },
+  },
+  generates: {
+    'src/': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        folder: '../../src/__graphql__/leaderboard',
+        extensions: '.generated.ts',
+        baseTypesPath: '__graphql__/base-types.ts',
+      },
+      config: {
+        importOperationTypesFrom: 'Types',
+      },
+      plugins: ['typescript-document-nodes'],
+    },
+  },
+};
+
+export default config;
--- packages/network-query/scripts/codegen.sh
+++ packages/network-query/scripts/codegen.sh
@@ -6,6 +6,9 @@ graphql-codegen --require dotenv/config --config types.codegen.ts
 # exchange
 graphql-codegen --require dotenv/config --config exchange.codegen.ts
 
+# leadboard
+graphql-codegen --require dotenv/config --config leaderboard.codegen.ts
+
 # network
 graphql-codegen --require dotenv/config --config network.codegen.ts
 
