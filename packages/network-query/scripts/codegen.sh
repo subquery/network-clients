d543730fe74d07d787454d553792b47eb731daa1
HZ <zhaihaohk@gmail.com>
2023-04-12 14:24:44 +1200

    chaneg network to use testnet for test purpose

--- packages/network-query/scripts/codegen.sh
+++ packages/network-query/scripts/codegen.sh
@@ -6,8 +6,8 @@ graphql-codegen --config types.codegen.ts
 # exchange
 graphql-codegen --config exchange.codegen.ts
 
-# network
-# graphql-codegen --config network.codegen.ts
+network
+graphql-codegen --config network.codegen.ts
 
 yarn prettier --write . 
 
