6e7797d3e12d8c5fc603bf0bc5efcf1aa0cec82a
Ian He <39037239+ianhe8x@users.noreply.github.com>
2023-06-13 12:03:08 +1200

    fix the apollo/client import to /core to avoid react dep

--- packages/apollo-links/src/fallbackLink.ts
+++ packages/apollo-links/src/fallbackLink.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ApolloLink, Operation, NextLink, Observable, FetchResult } from '@apollo/client';
+import { ApolloLink, Operation, NextLink, Observable, FetchResult } from '@apollo/client/core';
 
 export class FallbackLink extends ApolloLink {
   constructor(private url?: string) {
