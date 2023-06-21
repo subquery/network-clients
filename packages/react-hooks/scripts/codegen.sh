5b1323778e97dceb51d8868ec75401ad58de7ad2
cyrbuzz <cyrbuzz@foxmail.com>
2023-06-21 13:01:43 +0800

    feat: add react hooks

--- packages/react-hooks/scripts/codegen.sh
+++ packages/react-hooks/scripts/codegen.sh
@@ -6,6 +6,9 @@ graphql-codegen --require dotenv/config --config exchange.codegen.ts
 # network
 graphql-codegen --require dotenv/config --config network.codegen.ts
 
+# leadboard
+graphql-codegen --require dotenv/config --config leadboard.codegen.ts
+
 yarn prettier --write . 
 
 # creating export files
