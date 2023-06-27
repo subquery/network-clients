6786e0e2711d22ffc8143d3c7c478cefa676408e
HZHAIX <>
2023-06-28 11:35:40 +1200

    Add `orderType` to link init option

--- packages/apollo-links/src/types.ts
+++ packages/apollo-links/src/types.ts
@@ -1,6 +1,11 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+export enum OrderType {
+  dictionary = 'dictionary',
+  deployment = 'deployment',
+}
+
 export type Agreement = {
   id: string;
   indexer: string;
