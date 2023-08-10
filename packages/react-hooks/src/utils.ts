8fe6906a6a4aacef09d98229e1f9d39b841ab976
cyrbuzz <cyrbuzz@foxmail.com>
2023-08-10 10:32:18 +0800

    chore: move to clients

--- packages/react-hooks/src/utils.ts
+++ packages/react-hooks/src/utils.ts
@@ -29,20 +29,6 @@ export function mapAsync<O, T>(scope: (t: T) => O, data: AsyncData<T>): AsyncDat
   return { ...data, data: data.data ? scope(data.data) : undefined };
 }
 
-export const indexingProgress = ({
-  currentHeight,
-  targetHeight,
-  startHeight = 0,
-}: {
-  currentHeight: number;
-  targetHeight: number;
-  startHeight: number;
-}) => {
-  if (targetHeight === startHeight) return 0;
-  const rawProgress = (currentHeight - startHeight) / (targetHeight - startHeight);
-  return isNaN(rawProgress) ? 0 : Math.min(Math.max(rawProgress, 0), 1);
-};
-
 export type RenderResult = ReactElement | null;
 
 export type Handlers<T> = {
