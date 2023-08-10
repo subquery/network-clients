abb5601248639178567a0645dd865ef1bd7a3e0e
Cyrbuzz <cyrbuzz@foxmail.com>
2023-08-10 11:19:04 +0800

    feat: add calc progress number (#166)
    * feat: add calc progress number
    
    * chore: fix name
    
    * chore: move to clients
    
    * chore: adjust

--- packages/network-clients/src/utils/index.ts
+++ packages/network-clients/src/utils/index.ts
@@ -8,3 +8,17 @@ export * from './ipfs';
 export function min(a: BigNumber, b: BigNumber): BigNumber {
   return a.lte(b) ? a : b;
 }
+
+export const indexingProgress = ({
+  currentHeight,
+  targetHeight,
+  startHeight = 0,
+}: {
+  currentHeight: number;
+  targetHeight: number;
+  startHeight: number;
+}) => {
+  if (targetHeight === startHeight) return 0;
+  const rawProgress = (currentHeight - startHeight) / (targetHeight - startHeight);
+  return isNaN(rawProgress) ? 0 : Math.min(Math.max(rawProgress, 0), 1);
+};
--- packages/react-hooks/src/renderAsync.ts
+++ packages/react-hooks/src/renderAsync.ts
@@ -5,17 +5,17 @@ import { AsyncData } from './types';
 import { Handlers, HandlersArray, RenderResult } from './utils';
 
 export function renderAsync<T>(data: AsyncData<T>, handlers: Handlers<T>): RenderResult {
-  if (data.data !== undefined) {
+  if (data.error) {
+    return handlers.error(data.error);
+  } else if (data.loading) {
+    return handlers.loading();
+  } else if (data.data !== undefined) {
     try {
       return handlers.data(data.data, data);
     } catch (e) {
       // TODO not sure this is desired behaviour
       return handlers.error(e as Error);
     }
-  } else if (data.error) {
-    return handlers.error(data.error);
-  } else if (data.loading) {
-    return handlers.loading();
   }
 
   return null;
@@ -25,6 +25,11 @@ export function renderAsyncArray<T extends any[]>(
   data: AsyncData<T>,
   handlers: HandlersArray<T>
 ): RenderResult {
+  if (data.error) {
+    return handlers.error(data.error);
+  } else if (data.loading) {
+    return handlers.loading();
+  }
   if (data.data !== undefined) {
     try {
       if (data.data === null || (Array.isArray(data.data) && !data.data.length)) {
@@ -35,11 +40,6 @@ export function renderAsyncArray<T extends any[]>(
       // TODO not sure this is desired behaviour
       return handlers.error(e as Error);
     }
-  }
-  if (data.error) {
-    return handlers.error(data.error);
-  } else if (data.loading) {
-    return handlers.loading();
   }
 
   return null;
