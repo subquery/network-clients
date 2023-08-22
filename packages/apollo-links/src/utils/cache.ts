953142cdc41eec5208e37003b96776b3595c354c
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-08-23 11:52:15 +1200

    Add cache support `localstorage` and `lru-cache`

--- packages/apollo-links/src/utils/cache.ts
+++ packages/apollo-links/src/utils/cache.ts
@@ -1,32 +1,81 @@
 // Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export interface CacheTool {
+import { LRUCache as LRU } from 'lru-cache';
+
+export interface ICache {
   get<T>(key: string): T | undefined;
-  set<T>(key: string, value: T): void;
+  set<T>(key: string, value: T): void; // ttl in milliseconds
   remove(key: string): void;
-  clear(): void;
 }
 
-export class MemoryCache implements CacheTool {
-  private store: { [key: string]: any } = {};
+interface Options {
+  ttl: number;
+}
+
+export class LocalStorageCache implements ICache {
+  private ttl: number;
+
+  constructor(options: Options) {
+    this.ttl = options.ttl;
+  }
 
   get<T>(key: string): T | undefined {
-    if (key in this.store) {
-      return this.store[key] as T;
+    const data = localStorage.getItem(key);
+    if (!data) return undefined;
+
+    try {
+      const parsed = JSON.parse(data);
+      if (parsed.expiry && parsed.expiry < Date.now()) {
+        localStorage.removeItem(key);
+        return undefined;
+      }
+
+      return parsed.value;
+    } catch {
+      return undefined;
     }
-    return undefined;
   }
 
   set<T>(key: string, value: T): void {
-    this.store[key] = value;
+    const data = {
+      value,
+      expiry: this.ttl ? Date.now() + this.ttl : undefined,
+    };
+    localStorage.setItem(key, JSON.stringify(data));
   }
 
   remove(key: string): void {
-    delete this.store[key];
+    localStorage.removeItem(key);
+  }
+}
+
+export class LRUCache implements ICache {
+  private cache: LRU<string, any>;
+
+  constructor(options: Options) {
+    this.cache = new LRU({ max: 1000, ttl: options.ttl });
   }
 
-  clear(): void {
-    this.store = {};
+  get<T>(key: string): T | undefined {
+    return this.cache.get(key);
   }
+
+  set<T>(key: string, value: T, ttl?: number): void {
+    // If ttl is defined, it is passed in milliseconds.
+    // lru-cache expects ttl in milliseconds as well, so it aligns perfectly.
+    this.cache.set(key, value, { ttl });
+  }
+
+  remove(key: string): void {
+    this.cache.delete(key);
+  }
+}
+
+export function initCache(options: Options): ICache {
+  if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+    return new LocalStorageCache(options);
+  }
+
+  return new LRUCache(options);
 }
