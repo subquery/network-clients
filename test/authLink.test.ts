6508de73a5bb1b0d84281813911fe49dc92ee50c
cyrbuzz <cyrbuzz@foxmail.com>
2023-11-06 18:18:00 +0800

    chore: remove skip

--- test/authLink.test.ts
+++ test/authLink.test.ts
@@ -235,7 +235,7 @@ describe('auth link', () => {
   });
 });
 
-describe.only('mock: auth link with auth center', () => {
+describe('mock: auth link with auth center', () => {
   let client: ApolloClient<unknown>;
   const authUrl = process.env.AUTH_URL ?? 'https://kepler-auth.subquery.network';
   const chainId = '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3';
