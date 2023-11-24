58c549c49da9e5b75fc9f3ee6c2290a8ad93d2e6
cyrbuzz <cyrbuzz@foxmail.com>
2023-11-24 09:37:41 +0800

    fix: change to testnet

--- packages/network-query/leaderboard.codegen.ts
+++ packages/network-query/leaderboard.codegen.ts
@@ -5,7 +5,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 import { NETWORK_CONFIGS } from '@subql/network-config';
 
 const config: CodegenConfig = {
-  schema: NETWORK_CONFIGS.kepler.gql.leaderboard,
+  schema: NETWORK_CONFIGS.testnet.gql.leaderboard,
   documents: './queries/leaderboard/*.gql',
   config: {
     preResolveTypes: true,
