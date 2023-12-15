39c6bc7901b788cb374f38247c160f477e00e321
cyrbuzz <cyrbuzz@foxmail.com>
2023-12-15 15:36:43 +0800

    feat: change leader board

--- packages/network-query/leaderboard.codegen.ts
+++ packages/network-query/leaderboard.codegen.ts
@@ -5,7 +5,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 import { NETWORK_CONFIGS } from '@subql/network-config';
 
 const config: CodegenConfig = {
-  schema: NETWORK_CONFIGS.kepler.gql.leaderboard,
+  schema: NETWORK_CONFIGS.mainnet.gql.leaderboard,
   documents: './queries/leaderboard/*.gql',
   config: {
     preResolveTypes: true,
