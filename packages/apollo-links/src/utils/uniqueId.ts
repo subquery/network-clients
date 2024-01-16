7546a1a23c9ba908b70f323298c6c4552945e927
Jacob <princewith@gmail.com>
2024-01-16 10:46:22 +0800

    update unique id generator

--- packages/apollo-links/src/utils/uniqueId.ts
+++ packages/apollo-links/src/utils/uniqueId.ts
@@ -1,10 +1,6 @@
 // Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ethers } from 'ethers';
-
-export function generateUniqueId() {
-  const randomBytes = ethers.utils.randomBytes(16);
-  const uniqueId = ethers.utils.keccak256(randomBytes);
-  return uniqueId;
+export function generateUniqueId(length = 32) {
+  return Array.from(Array(length), () => Math.floor(Math.random() * 36).toString(36)).join('');
 }
