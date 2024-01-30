f28fc778fba38269d3b79c01c1e540fb168aa46c
mzxyz <8177474+mzxyz@users.noreply.github.com>
2024-01-31 12:34:25 +1300

    Add fun to `getIndexerMetadata` only (#250)
    * Add func to `getIndexerMetadata` only
    
    * Fix test for `networkClientSDK`

--- test/networkClient.test.ts
+++ test/networkClient.test.ts
@@ -1,14 +1,12 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { SQNetworks } from '@subql/network-config';
+import { SQNetworks } from '../packages/network-config/src';
 import { NetworkClient } from '../packages/network-clients/src';
 
-const TEST_INDEXER = '0xFCA0037391B3cfe28f17453D6DBc4A7618F771e1';
+const TEST_INDEXER = '0xCef192586b70e3Fc2FAD76Dd1D77983a30d38D04';
 
-// testnet have change, skip for now
-// FIXME
-describe.skip('network client', () => {
+describe('network client', () => {
   let client: NetworkClient;
   beforeAll(() => {
     client = NetworkClient.create(SQNetworks.TESTNET);
@@ -17,6 +15,12 @@ describe.skip('network client', () => {
   it('can get indexer detail', async () => {
     const indexer = await client.getIndexer(TEST_INDEXER);
     expect(indexer?.metadata).toBeTruthy();
+  });
+
+  it('can get indexer metadata', async () => {
+    const metadata = await client.getIndexerMetadata(TEST_INDEXER);
+    expect(metadata?.name).toBeTruthy();
+    expect(metadata?.url).toBeTruthy();
   });
 
   it('can get delegating value', async () => {
