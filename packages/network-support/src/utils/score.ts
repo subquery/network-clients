acaf9aa0edb0cda95cdcb768e40dcf8637cf65fd
luke <123917244@qq.com>
2024-06-04 17:45:16 +0800

    feat: refactor

--- packages/network-support/src/utils/score.ts
+++ packages/network-support/src/utils/score.ts
@@ -14,53 +14,35 @@ export enum CurveType {
 export type IndexerHeight = {
   indexer: string;
   height: number;
+  rawHeight: number;
 };
 
 export async function updateBlockScoreWeight(
   scoreStore: IStore,
   deploymentId: string,
-  heights: IndexerHeight[][],
+  heights: IndexerHeight[],
   logger?: any
 ) {
-  const len = heights.length;
-
-  const avgs = [];
-  let effectiveAvgIndx = -1;
   let minHeight = Number.MAX_SAFE_INTEGER;
   let maxHeight = 0;
-  for (let i = 0; i < len; i++) {
-    const h = heights[i];
-    let sum = 0;
-    for (const { height } of h) {
-      sum += height;
-      minHeight = Math.min(minHeight, height);
-      maxHeight = Math.max(maxHeight, height);
-    }
-    const avg = Math.floor(sum / h.length);
-    avgs.push(avg);
-    effectiveAvgIndx = avg ? i : effectiveAvgIndx;
+  for (const { height } of heights) {
+    minHeight = Math.min(minHeight, height);
+    maxHeight = Math.max(maxHeight, height);
   }
 
   const key = getBlockScoreKey();
-  for (let i = 0; i < len; i++) {
-    for (const { indexer, height } of heights[i]) {
-      const delta = i < effectiveAvgIndx ? Math.abs(avgs[i] - avgs[effectiveAvgIndx]) : 0;
-      let weight = scoreMap(
-        height + delta,
-        [minHeight, maxHeight],
-        BLOCK_WEIGHT_OUTPUT_RANGE,
-        CurveType.QUADRATIC
-      );
-      weight = Math.floor(weight * 10) / 10;
-      await scoreStore.set(`${key}:${indexer}_${deploymentId}`, weight);
-      logger?.debug(
-        `${deploymentId}(minH:${minHeight}, maxH:${maxHeight}, batch:${len}, curBatch:${
-          i + 1
-        } effectiveAvgIndx:${effectiveAvgIndx}, effectiveAvg:${
-          avgs[effectiveAvgIndx]
-        }) set ${indexer} height:${height} delta:${delta} to ${weight}`
-      );
-    }
+  for (const { indexer, height, rawHeight } of heights) {
+    let weight = scoreMap(
+      height,
+      [minHeight, maxHeight],
+      BLOCK_WEIGHT_OUTPUT_RANGE,
+      CurveType.QUADRATIC
+    );
+    weight = Math.floor(weight * 10) / 10;
+    await scoreStore.set(`${key}:${indexer}_${deploymentId}`, weight);
+    logger?.debug(
+      `${deploymentId}(minH:${minHeight}, maxH:${maxHeight}) set ${indexer}(rawHeight:${rawHeight}) height:${height} to ${weight}`
+    );
   }
 }
 
