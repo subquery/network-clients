e451f57342dc9f33460973f15c18a05094fef41c
Scott Twiname <skott.twiname@gmail.com>
2025-10-15 14:54:33 +1300

    Remove and clean up network-clients package dependencies (#365)
    * Remove and clean up network-clients package dependencies
    
    * Clean up buffer import
    
    * Update changelog
    
    * bump nodejs version for CI

--- packages/network-clients/CHANGELOG.md
+++ packages/network-clients/CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Removed
+- Unused `axios`, `cross-fetch` and `buffer` dependencies (#365)
 
 ## [1.1.0] - 2024-02-21
 
--- packages/network-clients/src/clients/ipfsClient.ts
+++ packages/network-clients/src/clients/ipfsClient.ts
@@ -1,11 +1,9 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import buffer from 'buffer';
+import { Buffer } from 'node:buffer';
 import { IPFSHTTPClient, create } from 'ipfs-http-client';
 import { concatU8A, isCID } from '../utils';
-
-const Buffer = buffer.Buffer;
 
 export class IPFSClient {
   private _client: IPFSHTTPClient;
